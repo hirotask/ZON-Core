1de909bde45f4899e571522f82010acb7c6543b4
hirotask <hirotaskhashi@gmail.com>
2023-06-18 23:00:33 +0900

    [fix]: イベントクラスの修正

--- src/main/kotlin/com/github/hirotask/event/ZombieDeathByPlayerEvent.kt
+++ src/main/kotlin/com/github/hirotask/event/ZombieDeathByPlayerEvent.kt
@@ -2,15 +2,31 @@ package com.github.hirotask.event
 
 import org.bukkit.entity.Player
 import org.bukkit.entity.Zombie
+import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
-class ZombieDeathByPlayerEvent(val player: Player, val zombie: Zombie) : Event() {
-    companion object {
-        val handlers = HandlerList()
-    }
+class ZombieDeathByPlayerEvent(val player: Player, val zombie: Zombie) : Event(), Cancellable {
+    var cancel: Boolean = false
 
     override fun getHandlers(): HandlerList {
-        return PlayerAttackZombieEvent.handlers
+        return HANDLERS
+    }
+
+    override fun isCancelled(): Boolean {
+        return cancel
+    }
+
+    override fun setCancelled(p0: Boolean) {
+        cancel = p0
+    }
+
+    companion object {
+        private val HANDLERS = HandlerList()
+
+        @JvmStatic
+        fun getHandlerList(): HandlerList {
+            return HANDLERS
+        }
     }
 }
