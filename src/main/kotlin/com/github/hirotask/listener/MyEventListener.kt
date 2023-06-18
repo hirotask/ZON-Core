b4b933fdceb604820c5dcf507fca63ca2e01fe90
hirotask <hirotaskhashi@gmail.com>
2023-06-19 00:39:27 +0900

    [add]: 報酬の追加

--- src/main/kotlin/com/github/hirotask/listener/MyEventListener.kt
+++ src/main/kotlin/com/github/hirotask/listener/MyEventListener.kt
@@ -5,22 +5,33 @@ import com.github.hirotask.event.PlayerAttackZombieEvent
 import com.github.hirotask.event.ZombieDeathByPlayerEvent
 import com.github.hirotask.infra.DAO
 import com.github.syari.spigot.api.event.events
+import com.github.syari.spigot.api.sound.playSound
+import org.bukkit.Material
+import org.bukkit.Sound
+import org.bukkit.inventory.ItemStack
 
 object MyEventListener {
     fun register() {
         Main.INSTANCE.events {
-            event<PlayerAttackZombieEvent> {
-                val player = it.player
-                val zombie = it.zombie
-            }
             event<ZombieDeathByPlayerEvent> {
                 val player = it.player
 
-//                player.playSound(Sound.ENTITY_ENDER_DRAGON_DEATH, volume = 0.5F)
-
                 val dao = DAO()
                 dao.insertPlayer(player)
-                player.sendMessage("Kills: ${dao.getKills(player)}")
+
+                when(dao.getKills(player)) {
+                    50 -> {
+                        player.sendMessage("50キル達成！")
+                        player.inventory.addItem(ItemStack(Material.DIAMOND))
+                        player.playSound(Sound.ENTITY_ENDER_DRAGON_DEATH, volume=0.5F)
+                    }
+                    100 -> {
+                        player.sendMessage("100キル達成！")
+                        player.inventory.addItem(ItemStack(Material.NETHER_STAR))
+                        player.playSound(Sound.ENTITY_ENDER_DRAGON_DEATH, volume=0.5F)
+                    }
+                }
+
             }
         }
     }
