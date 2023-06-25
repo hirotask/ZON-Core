3b21bc17006c09c139e60f576191094e13c23f74
hirotask <hirotaskhashi@gmail.com>
2023-06-26 00:05:44 +0900

    [fix]: ZONPlayerクラスをデータクラスに変更

--- src/main/kotlin/com/github/hirotask/domain/ZONPlayer.kt
+++ src/main/kotlin/com/github/hirotask/domain/ZONPlayer.kt
@@ -2,4 +2,4 @@ package com.github.hirotask.domain
 
 import org.bukkit.entity.Player
 
-class ZONPlayer(val player: Player, var zombieKillCount: Int, var statusPoint: Int)
+data class ZONPlayer(val player: Player, var zombieKillCount: Int, var statusPoint: Int)
