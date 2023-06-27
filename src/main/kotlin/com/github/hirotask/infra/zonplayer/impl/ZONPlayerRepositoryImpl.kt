b3d33be6dee42b77f5f956cee89b982c2dd92522
hirotask <hirotaskhashi@gmail.com>
2023-06-27 19:37:10 +0900

    [fix]: SQL文の修正

--- src/main/kotlin/com/github/hirotask/infra/zonplayer/impl/ZONPlayerRepositoryImpl.kt
+++ src/main/kotlin/com/github/hirotask/infra/zonplayer/impl/ZONPlayerRepositoryImpl.kt
@@ -79,24 +79,26 @@ class ZONPlayerRepositoryImpl(val database: Database) : ZONPlayerRepository {
 
     override fun getZONPlayer(player: Player): ZONPlayer {
         database.connect()
-        val sql = "SELECT COUNT(*) kills, SUM(dsp_diff) status_point FROM ms_players INNER JOIN dt_player_kills ON ms_players.mp_id = dt_player_kills.mp_id INNER JOIN dt_status_points ON ms_players.mp_id = dt_status_points.mp_id WHERE mp_name = '${player.name}'"
+        val sql = "SELECT COUNT(dpk_id) kills, SUM(dsp_diff) status_point FROM dt_player_kills LEFT JOIN dt_status_points ON dt_player_kills.mp_id = dt_status_points.mp_id LEFT JOIN ms_players ON dt_player_kills.mp_id = ms_players.mp_id WHERE mp_name = '${player.name}'"
         val rs = database.select(sql) ?: throw Exception()
 
+        var kills = 0
+        var statusPoint = 0
         if (rs.first()) {
-            if (!rs.wasNull()) {
-                val kills = rs.getInt("kills")
-                val statusPoint = rs.getInt("status_point")
-                database.disconnect()
-
-                return ZONPlayer(
-                    player = player,
-                    zombieKillCount = kills,
-                    statusPoint = statusPoint
-                )
+            if (rs.wasNull()) {
+                throw ZONPlayerNotFoundException("player: ${player.name} is not found in DB")
+            } else {
+                kills = rs.getInt("kills")
+                statusPoint = rs.getInt("status_point")
             }
         }
+        database.disconnect()
 
-        throw ZONPlayerNotFoundException("player: ${player.name} is not found in DB")
+        return ZONPlayer(
+            player = player,
+            zombieKillCount = kills,
+            statusPoint = statusPoint
+        )
     }
 
     override fun addZONPlayer(player: Player): Int {
