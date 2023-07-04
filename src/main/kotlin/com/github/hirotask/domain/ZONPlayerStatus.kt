aea4d93e66971526ce30e0dcc4f3669da4d00200
hirotask <hirotaskhashi@gmail.com>
2023-07-04 22:06:30 +0900

    [update]: ZONPlayerStatusをエンティティにした

--- src/main/kotlin/com/github/hirotask/domain/ZONPlayerStatus.kt
+++ src/main/kotlin/com/github/hirotask/domain/ZONPlayerStatus.kt
@@ -1,3 +1,3 @@
 package com.github.hirotask.domain
 
-data class ZONPlayerStatus(val hp: Int, val hpRegen: Int, val mp: Int, val mpRegen: Int, val strength: Int)
+data class ZONPlayerStatus(var hp: Int, var hpRegen: Int, var mp: Int, var mpRegen: Int, var strength: Int)
