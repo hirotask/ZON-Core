59bbdcd85e4af15adc215b5f14762056309db954
hirotask <hirotaskhashi@gmail.com>
2023-07-06 00:29:41 +0900

    [update]: UseCaseのコメントを追加

--- src/main/kotlin/com/github/hirotask/usecase/ReinforceStatusUseCase.kt
+++ src/main/kotlin/com/github/hirotask/usecase/ReinforceStatusUseCase.kt
@@ -2,15 +2,44 @@ package com.github.hirotask.usecase
 
 import org.bukkit.entity.Player
 
+/**
+ * ステータス強化に関するユースケース
+ *
+ */
 interface ReinforceStatusUseCase {
 
+    /**
+     * HPを強化する
+     *
+     * @param player 強化するプレイヤー
+     */
     fun reinforceHP(player: Player)
 
+    /**
+     * HP再生速度を強化する
+     *
+     * @param player 強化するプレイヤー
+     */
     fun reinforceHPRegen(player: Player)
 
+    /**
+     * MPを強化する
+     *
+     * @param player 強化するプレイヤー
+     */
     fun reinforceMP(player: Player)
 
+    /**
+     * MP再生速度を強化する
+     *
+     * @param player 強化するプレイヤー
+     */
     fun reinforceMPRegen(player: Player)
 
+    /**
+     * 攻撃力を強化する
+     *
+     * @param player 強化するプレイヤー
+     */
     fun reinforceStrength(player: Player)
 }
