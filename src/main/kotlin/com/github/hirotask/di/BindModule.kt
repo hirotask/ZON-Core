c0b546b60ee444b27f31044e9516fc5a257f4a63
hirotask <hirotaskhashi@gmail.com>
2023-07-05 16:39:29 +0900

    [update]: UseCaseのDIの設定

--- src/main/kotlin/com/github/hirotask/di/BindModule.kt
+++ src/main/kotlin/com/github/hirotask/di/BindModule.kt
@@ -5,9 +5,13 @@ import com.github.hirotask.domain.ZONPlayerServiceImpl
 import com.github.hirotask.usecase.AddStatusPointUseCase
 import com.github.hirotask.usecase.AddZombieKillsUseCase
 import com.github.hirotask.usecase.InitZONPlayerUseCase
+import com.github.hirotask.usecase.ReinforceStatusUseCase
+import com.github.hirotask.usecase.ZONPlayerActionUseCase
 import com.github.hirotask.usecase.impl.AddStatusPointUseCaseImpl
 import com.github.hirotask.usecase.impl.AddZombieKillsUseCaseImpl
 import com.github.hirotask.usecase.impl.InitZONPlayerUseCaseImpl
+import com.github.hirotask.usecase.impl.ReinforceStatusUseCaseImpl
+import com.github.hirotask.usecase.impl.ZONPlayerActionUseCaseImpl
 import dagger.Binds
 import dagger.Module
 import javax.inject.Singleton
@@ -24,6 +28,9 @@ interface BindModule {
     fun bindsZONPlayerService(impl: ZONPlayerServiceImpl): ZONPlayerService
 
     @Binds
+    fun bindsZONPlayerActionUseCase(impl: ZONPlayerActionUseCaseImpl) : ZONPlayerActionUseCase
+
+    @Binds
     fun bindsAddStatusPointUseCase(impl : AddStatusPointUseCaseImpl) : AddStatusPointUseCase
 
     @Binds
@@ -31,4 +38,8 @@ interface BindModule {
 
     @Binds
     fun bindsInitZONPlayerUseCase(impl: InitZONPlayerUseCaseImpl): InitZONPlayerUseCase
+
+    @Binds
+    fun bindsReinforceStatusUseCase(impl: ReinforceStatusUseCaseImpl): ReinforceStatusUseCase
+
 }
