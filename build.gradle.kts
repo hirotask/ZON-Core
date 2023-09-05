6d40779a33f8e185b582b3146ce144fa479e592b
hirotask <hirotaskhashi@gmail.com>
2023-09-06 00:18:47 +0900

    [fix]: dokkaによるドキュメント生成の修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,20 @@ plugins {
     kotlin("kapt") version "1.8.20" apply false
     id("org.jmailen.kotlinter") version "3.8.0" apply false
     id("com.palantir.git-version") version "0.12.3" apply false
-    id("org.jetbrains.dokka") version "1.8.20" apply true
+    id("org.jetbrains.dokka") version "1.8.20"
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
 
 subprojects {
     apply(plugin = "com.palantir.git-version")
+    apply(plugin = "org.jetbrains.dokka")
 }
 
-tasks.getByName<org.jetbrains.dokka.gradle.DokkaTask>("dokkaHtml") {
-    outputDirectory.set(buildDir.resolve("dokkaHtmlOutput"))
+tasks.getByName<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>("dokkaHtmlMultiModule") {
+    outputDirectory.set(buildDir.resolve("dokkaHtmlMultiModuleOutput"))
 }
