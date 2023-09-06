a86b3beb2dae2f1e3e6e09046d4f40d02aed3491
hirotask <hirotaskhashi@gmail.com>
2023-09-06 13:59:32 +0900

    [add]: RepositoryのFakeを追加

---
+++ core/src/test/java/testsupport/FakeZONPlayerRepository.kt
@@ -0,0 +1,38 @@
+package testsupport
+
+import com.github.hirotask.core.domain.ZONPlayer
+import com.github.hirotask.core.domain.ZONPlayerStatus
+import com.github.hirotask.core.domain.repository.ZONPlayerRepository
+
+class FakeZONPlayerRepository: ZONPlayerRepository {
+    override fun updatePlayerName(zonPlayer: ZONPlayer, uuid: String): Int {
+        return 1
+    }
+
+    override fun addZombieKills(zonPlayer: ZONPlayer, amount: Int): Int {
+        zonPlayer.zombieKillCount += amount
+        return zonPlayer.zombieKillCount
+    }
+
+    override fun addStatusPoint(zonPlayer: ZONPlayer, amount: Int): Int {
+        zonPlayer.statusPoint += amount
+        return zonPlayer.statusPoint
+    }
+
+    override fun getZombieKills(zonPlayer: ZONPlayer): Int {
+        return zonPlayer.zombieKillCount
+    }
+
+    override fun getStatusPoint(zonPlayer: ZONPlayer): Int {
+        return zonPlayer.statusPoint
+    }
+
+    override fun getZONPlayer(playerName: String, playerUUID: String): ZONPlayer {
+        val status = ZONPlayerStatus(1,1,1,1,1)
+        return ZONPlayer(playerName, playerUUID, 1, 1, status)
+    }
+
+    override fun addZONPlayer(playerName: String, playerUUID: String): Int {
+        return 1
+    }
+}
---
+++ core/src/test/java/testsupport/FakeZONPlayerStatusRepository.kt
@@ -0,0 +1,56 @@
+package testsupport
+
+import com.github.hirotask.core.domain.ZONPlayer
+import com.github.hirotask.core.domain.ZONPlayerStatus
+import com.github.hirotask.core.domain.repository.ZONPlayerStatusRepository
+
+class FakeZONPlayerStatusRepository: ZONPlayerStatusRepository {
+    override fun getZONPlayerStatus(zonPlayer: ZONPlayer): ZONPlayerStatus {
+        return ZONPlayerStatus(1,1,1,1,1)
+    }
+
+    override fun getHP(zonPlayer: ZONPlayer): Int {
+        return zonPlayer.zonplayerStatus.hp
+    }
+
+    override fun getHPRegen(zonPlayer: ZONPlayer): Int {
+        return zonPlayer.zonplayerStatus.hpRegen
+    }
+
+    override fun getMP(zonPlayer: ZONPlayer): Int {
+        return zonPlayer.zonplayerStatus.mp
+    }
+
+    override fun getMPRegen(zonPlayer: ZONPlayer): Int {
+        return zonPlayer.zonplayerStatus.mpRegen
+    }
+
+    override fun getStrength(zonPlayer: ZONPlayer): Int {
+        return zonPlayer.zonplayerStatus.strength
+    }
+
+    override fun addHP(zonPlayer: ZONPlayer, amount: Int): Int {
+        zonPlayer.zonplayerStatus.hp += amount
+        return zonPlayer.zonplayerStatus.hp
+    }
+
+    override fun addHPRegen(zonPlayer: ZONPlayer, amount: Int): Int {
+        zonPlayer.zonplayerStatus.hpRegen += amount
+        return zonPlayer.zonplayerStatus.hpRegen
+    }
+
+    override fun addMP(zonPlayer: ZONPlayer, amount: Int): Int {
+        zonPlayer.zonplayerStatus.mp += amount
+        return zonPlayer.zonplayerStatus.mp
+    }
+
+    override fun addMPRegen(zonPlayer: ZONPlayer, amount: Int): Int {
+        zonPlayer.zonplayerStatus.mpRegen += amount
+        return zonPlayer.zonplayerStatus.mpRegen
+    }
+
+    override fun addStrength(zonPlayer: ZONPlayer, amount: Int): Int {
+        zonPlayer.zonplayerStatus.strength += amount
+        return zonPlayer.zonplayerStatus.strength
+    }
+}
